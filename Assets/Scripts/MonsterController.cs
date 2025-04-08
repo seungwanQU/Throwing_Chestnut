b2bd92ea284afd3f580d901038ddc3c433e47675
swRyu <ryu0114@quantumuniverse.co>
2025-04-08 12:22:26 +0900

    feat : nextMonster Spawn

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -1,18 +1,33 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 
 public class MonsterController : MonoBehaviour
 {
     public float maxHealth = 100f;
     public float currentHealth = 100f;
+    public float delay = 3f;   // 몬스터 destroy 후 대기 시간
 
-    public Animator animator;
     public Slider healthSlider;
+    public Slider healthSliderCopy;
     public TextMeshProUGUI textHP;
+    public TextMeshProUGUI textHPCopy;
+
+    public GameObject monsterPosition;
+    public GameObject currentMonsterPrefab;
+    public GameObject nextMonsterPrefab;
+
+    private Animator animator;
 
     void Start()
     {
+        animator = currentMonsterPrefab.GetComponent<Animator>();
+
+        healthSliderCopy = this.healthSlider;
+        textHPCopy = this.textHP;
+
         UpdateHealthSlider();
     }
 
@@ -22,7 +37,22 @@ public class MonsterController : MonoBehaviour
 
         if (currentHealth <= 0f)
         {
+            currentHealth = maxHealth;
+
             animator.SetBool("Death", true);
+
+            Invoke("DestroyMonster", delay);
+
+            if (nextMonsterPrefab != null)
+            {
+                Instantiate(nextMonsterPrefab, monsterPosition.transform.position, monsterPosition.transform.rotation);
+                this.GetComponent<MonsterController>().healthSlider = this.healthSliderCopy;
+                this.GetComponent<MonsterController>().textHP = this.textHPCopy;
+                this.GetComponent<MonsterController>().maxHealth = this.maxHealth;
+                this.GetComponent<MonsterController>().currentHealth = this.currentHealth;
+                this.GetComponent<MonsterController>().UpdateHealthSlider();
+                this.GetComponent<MonsterController>().IncreaseHealth();
+            }
         }
 
         UpdateHealthSlider();
@@ -33,14 +63,22 @@ public class MonsterController : MonoBehaviour
         float decreaseHp = (float)currentHealth / (float)maxHealth;
 
         healthSlider.value = decreaseHp;
+        textHP.text = $"{currentHealth} / {maxHealth}";
+    }
 
-        if (currentHealth <= 0f)
+    public void IncreaseHealth()
+    {
+        maxHealth = maxHealth * 2.0f;
+
+        if (currentHealth != maxHealth)
         {
-            textHP.text = $"0 / {maxHealth}";
+            currentHealth = maxHealth;
+            UpdateHealthSlider();
         }
-        else
-        {
-            textHP.text = $"{currentHealth} / {maxHealth}";
-        }
+    }
+
+    private void DestroyMonster()
+    {
+        Destroy(currentMonsterPrefab);
     }
 }

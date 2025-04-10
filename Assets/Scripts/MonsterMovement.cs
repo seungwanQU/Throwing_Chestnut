7ff8064e82dda1e45c8c86cf8d0e75d14e462a14
swRyu <ryu0114@quantumuniverse.co>
2025-04-10 13:46:20 +0900

    feat : NavMesh 관련 Setting & Script 진행

---
+++ Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class MonsterMovement : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+
+    private bool isMoving = false;
+
+    [SerializeField] private float minMoveDelay = 1f; // 최소 이동 딜레이
+    [SerializeField] private float maxMoveDelay = 3f; // 최대 이동 딜레이
+
+    private void Awake()
+    {
+        navMeshAgent = navMeshAgent.GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        // 처음에 이동을 시작하도록 호출
+        StartCoroutine(MoveCoroutine());
+    }
+
+    private IEnumerator MoveCoroutine()
+    {
+        while (true)
+        {
+            if (!isMoving)
+            {
+                // 이동을 시작하기 전에 랜덤한 딜레이를 줍니다.
+                float moveDelay = Random.Range(minMoveDelay, maxMoveDelay);
+                yield return new WaitForSeconds(moveDelay);
+
+                // 새로운 목표 지점을 선택하여 이동합니다.
+                Vector3 targetPosition = GetRandomTargetPosition();
+                MoveTo(targetPosition);
+            }
+            yield return null;
+        }
+    }
+
+    private Vector3 GetRandomTargetPosition()
+    {
+        // 랜덤한 위치를 선택하여 반환합니다.
+        Vector3 randomPosition = new Vector3(Random.Range(555f, 585f), 0f, Random.Range(335f, 375f));
+        return randomPosition;
+    }
+
+    private void MoveTo(Vector3 targetPosition)
+    {
+        isMoving = true;
+        navMeshAgent.SetDestination(targetPosition);
+    }
+
+    private void Update()
+    {
+        if (isMoving && navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        {
+            // 이동이 완료되었을 때 이동 상태를 해제합니다.
+            isMoving = false;
+        }
+    }
+}
